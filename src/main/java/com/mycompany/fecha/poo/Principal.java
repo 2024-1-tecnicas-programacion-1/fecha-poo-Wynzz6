5e3463c458779bd8134125d84eb043301c6f321b
Wynzz6 <141454169+Wynzz6@users.noreply.github.com>
2024-04-21 15:43:03 -0500

    implementación del codigo de la clase principal

--- src/main/java/com/mycompany/fecha/poo/Principal.java
+++ src/main/java/com/mycompany/fecha/poo/Principal.java
@@ -6,6 +6,28 @@ public class Principal {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el día:");
+        int dia = scanner.nextInt();
+
+        System.out.println("Ingrese el mes:");
+        int mes = scanner.nextInt();
+
+        System.out.println("Ingrese el año:");
+        int ano = scanner.nextInt();
+
+        Fecha fecha = new Fecha(dia, mes, ano);
+
+        if (fecha.validarFecha()) {
+            System.out.println("La fecha es válida.");
+            System.out.println("Formato de fecha larga: " + fecha.fechaLarga());
+        } else {
+            System.out.println("La fecha no es válida.");
+        }
+
+        scanner.close();
+    }
+}
         
         
          }
