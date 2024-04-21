ee4df94e6e044410c676ad12870df8d1d88691f3
Wynzz6 <141454169+Wynzz6@users.noreply.github.com>
2024-04-21 15:45:11 -0500

    .

--- src/main/java/com/mycompany/fecha/poo/Fecha.java
+++ src/main/java/com/mycompany/fecha/poo/Fecha.java
@@ -5,7 +5,7 @@ public class Fecha {
     int mes;
     int ano;
     
-    public Fecha(int dia, int mes, int ano){
+    public Fecha(int dia, int mes, int ano) {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
@@ -19,24 +19,25 @@ public class Fecha {
         if (mes < 1 || mes > 12) {
             return false;
         }
-        if (dia < 1 || ((mes == 2 && ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) && dia > 29) || 
+        if (dia < 1 || 
+            ((mes == 2 && ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0) && dia > 29) || 
             (mes == 2 && dia > 28)) || 
             ((mes == 4 || mes == 6 || mes == 9 || mes == 11) && dia > 30) ||
             dia > 31) {
-            return false;
+            return false; 
         }
         return true;
     }
-    public String mesLetra(){
+    
+    public String mesLetra() {
         String[] meses = {"enero", "febrero", "marzo", "abril" , "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
-        return meses[mes-1];
-        
+        return meses[mes - 1];
     }
-    public String fechaLarga(){
-        return dia + "de" + mesLetra() + "del" + ano; }
     
-    
+    public String fechaLarga() {
+        return dia + " de " + mesLetra() + " del " + ano;
     }
+}
 
 
     
